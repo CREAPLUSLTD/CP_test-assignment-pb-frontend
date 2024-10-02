0b2a3d1197bb68250bf625476a685377a87b41fd
oskarcokl <oskar.cokl@gmail.com>
2024-10-02 22:17:03 +0200

    feat: Made view more seponsive.

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,18 +1,17 @@
 <template>
-    <div class="w-5/6 mx-auto">
-        <h1 class="text-center text-4xl my-3">ListView</h1>
-        <div v-if="persons" class="flex flex-col items-center">
-            <label for="search">Search contacts</label>
-            <input type="text" name="search" class="border-sky-200 border-2 rounded-sm">
+    <div class="mx-auto">
+        <h1 class="text-center text-5xl my-3">Contacts</h1>
+        <div v-if="persons" class="flex flex-col items-center w-4/5 sm:w-2/5 sm:max-w-sm mx-auto">
+            <input type="text" placeholder="Search" name="search" class="border-slate-100 border-4 rounded-lg w-full text-2xl">
 
-            <div v-for="(person, index) in persons" :key="index" class="w-52 bg-slate-50 rounded-lg mt-2 px-3 py-2">
-                <div class="flex flex-row items-center gap-3">
-                    <div class="rounded-full bg-sky-300 w-1/3 aspect-square flex justify-center items-center">
-                        <span class="text-2xl">
+            <div v-for="(person, index) in persons" :key="index" class="bg-slate-50 w-full rounded-lg mt-3 px-3 py-2">
+                <div class="flex flex-row items-center gap-3 w-full">
+                    <div class="rounded-full bg-sky-300 w-1/4 aspect-square flex justify-center items-center">
+                        <span class="text-3xl">
                             {{ person.first_name.charAt(0) }}{{ person.last_name.charAt(0) }}
                         </span>
                     </div>
-                    <div>
+                    <div class="text-xl">
                         <h2>{{ person.first_name }} {{ person.last_name }}</h2>
                         <h2>{{ person.phone_number }}</h2>
                     </div>
