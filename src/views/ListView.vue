81229303e9cba521dbc6999f89e4e1fd5f728a85
oskarcokl <oskar.cokl@gmail.com>
2024-10-02 22:31:04 +0200

    feat: Add simple debounce search.

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -2,9 +2,9 @@
     <div class="mx-auto">
         <h1 class="text-center text-5xl my-3">Contacts</h1>
         <div v-if="persons" class="flex flex-col items-center w-4/5 sm:w-2/5 sm:max-w-sm mx-auto">
-            <input type="text" placeholder="Search" name="search" class="border-slate-100 border-4 rounded-lg w-full text-2xl">
+            <input v-model="searchQuery" @input="debounceSearch" type="text" placeholder="Search" name="search" class="border-slate-100 border-4 rounded-lg w-full text-2xl">
 
-            <div v-for="(person, index) in persons" :key="index" class="bg-slate-50 w-full rounded-lg mt-3 px-3 py-2">
+            <div v-for="(person, index) in filteredPersons" :key="index" class="bg-slate-50 w-full rounded-lg mt-3 px-3 py-2">
                 <div class="flex flex-row items-center gap-3 w-full">
                     <div class="rounded-full bg-sky-300 w-1/4 aspect-square flex justify-center items-center">
                         <span class="text-3xl">
@@ -38,12 +38,35 @@ export default defineComponent({
     name: 'ListView',
     data() {
         return {
-            persons: null as [Person] | null
+            persons: null as Person[] | null,
+            filteredPersons: null as Person[] | null,
+            searchQuery: "" as string,
+            debounceTimeout: undefined as NodeJS.Timeout | undefined
+        }
+    },
+    methods: {
+        debounceSearch() {
+            clearTimeout(this.debounceTimeout)
+
+            this.debounceTimeout = setTimeout(() => {
+                this.updateFilteredPersons();
+            }, 300)
+        },
+        updateFilteredPersons() {
+            if (!this.persons) {
+                return;
+            }
+
+            const query = this.searchQuery.toLowerCase();
+            this.filteredPersons = this.persons.filter(person => {
+                return person.first_name.toLowerCase().includes(query) || person.last_name.toLowerCase().includes(query)
+            })
         }
     },
     async created() {
         const res = await axios("persons.json");
         this.persons = res.data;
+        this.filteredPersons = this.persons
     }
 });
 </script>
