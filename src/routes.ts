de0028dab7527be1bbeb66b5fa72e2e2e198a3cd
oskarcokl <oskar.cokl@gmail.com>
2024-10-02 20:38:13 +0200

    feat: Add /list-view route

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import MainView from './views/MainView.vue';
+import MainView from '@/views/MainView.vue';
+import ListView from './views/ListView.vue';
 
 const routes = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const routes = createRouter({
       name: 'home',
       component: MainView,
     },
+    {
+      path: '/list-view',
+      name: 'lis-view',
+      component: ListView,
+    }
   ],
 });
 
---
+++ src/views/ListView.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>ListView</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ListView'
+});
+</script>
+
+<style>
+
+</style>
